8805af1d418e77a753f8558b1e1ce9721727a179
TrungDo <trungdo@TrungDos-MacBook-Air.local>
2021-10-21 17:00:08 +0700

    core: init project

---
+++ src/components/FriendManager.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Button } from 'reactstrap';
+
+export default function FriendManager() {
+    return (
+        <Button color="danger">Hi</Button>
+    )
+}
